7f97f5c29d5e9fc6cece3b9393171f99229c3cb2
KarolinaVaz <karolinavaz@hotmail.com>
2024-06-09 21:57:29 -0300

    Commit inicial curso de acessibilidade com reat

---
+++ src/app/_document.tsx
@@ -0,0 +1,25 @@
+import { axeAccessibilityReporter } from "@/utils/axeAccessibilityReporter";
+import { Html, Head, Main, NextScript } from "next/document";
+import { useEffect } from "react";
+
+export default function Document() {
+    useEffect(() => {
+        axeAccessibilityReporter()
+      },[])
+  return (
+    <Html>
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { axeAccessibilityReporter } from "@/utils/axeAccessibilityReporter";
+import { useEffect } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
